639fe4b148625373285761374960459baeb57557
Jenssile <jens.e.bouwens@gmail.com>
2025-06-13 02:28:04 +0200

    dry run flag and inplementation

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Default values
 VERBOSE=false
+DRY_RUN=false
 
 # Help message
 print_help() {
@@ -9,6 +10,7 @@ print_help() {
 	echo
 	echo "Options:"
 	echo "  -v, --verbose	Enable verbose output"
+	echo "  -d, --dry-run	Show what would be doen, without doing it"
 	echo "  -h, --help	Show this help message and exit"
 }
 
@@ -20,6 +22,7 @@ while [[ "$#" -gt 0 ]]; do
 			for (( i=1; i<${#1}; i++ )); do
 				case "${1:$i:1}" in
 					v) VERBOSE=true ;;
+					d) DRY_RUN=true ;;
 					h) print_help; exit 0;;
 					*) echo "Unknown option: -${1:$i:1}"; print_help; exit 1;;
 				esac
@@ -27,6 +30,9 @@ while [[ "$#" -gt 0 ]]; do
 			;;
 		--verbose)
 			VERBOSE=true
+			;;
+		--dry-run)
+			DRY_RUN=true
 			;;
 		--help)
 			print_help
@@ -45,8 +51,18 @@ while [[ "$#" -gt 0 ]]; do
 	shift
 done
 
+run_cmd() {
+	if $DRY_RUN; then
+		echo "[dry-run] $*"
+	else
+		eval "$@"
+	fi
+}
+
 if $VERBOSE; then
 	echo "Verbose output enabled."
 fi
 
-ehco "rest of scrit from here"
+echo "rest of scrit from here"
+
+run_cmd "touch ./test.txt"
