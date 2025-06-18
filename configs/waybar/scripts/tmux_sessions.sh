a3b7a82690cbea6fffb9ad40768b87f353856dc4
Jenssile <jens.e.bouwens@gmail.com>
2025-06-18 20:07:01 +0200

    updated it to not say tmux: on waybar

--- configs/waybar/scripts/tmux_sessions.sh
+++ configs/waybar/scripts/tmux_sessions.sh
@@ -10,9 +10,9 @@ sessions=$(tmux ls 2>/dev/null | cut -d: -f1 | head -n "$MAX_SESSIONS" | xargs)
 attached_count=$(tmux ls 2>/dev/null | grep -c "(attached)")
 
 if [ -z "$sessions" ]; then
-  echo '{"text": "tmux: none", "class": "tmux-none"}'
+  echo '{"text": "none", "class": "tmux-none"}'
 elif [ "$attached_count" -gt 0 ]; then
-  echo "{\"text\": \"tmux: $sessions\", \"class\": \"tmux-attached\"}"
+  echo "{\"text\": \"$sessions\", \"class\": \"tmux-attached\"}"
 else
-  echo "{\"text\": \"tmux: $sessions\", \"class\": \"tmux-detached\"}"
+  echo "{\"text\": \"$sessions\", \"class\": \"tmux-detached\"}"
 fi
